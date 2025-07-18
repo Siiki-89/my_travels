1b64381cb4c2a196a1efa349d802cf92be7b0dad
Alexandre de Melo <84542748+Siiki-89@users.noreply.github.com>
2025-07-18 18:32:22 -0300

    center title home

--- my_travels/lib/presentation/pages/home_page.dart
+++ my_travels/lib/presentation/pages/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
+      appBar: AppBar(title: Text('Home'), centerTitle: true),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pushNamed(context, '/addTravel');
